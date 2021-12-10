a52cd3abf4c83983ed3f6940590258508e36750d
Alex zhong <zhongwcool@163.com>
2021-12-10 19:57:18 +0800

    Add WpfApp1 which contains demo for LibVLCSharp.

---
+++ WpfApp1/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using LibVLCSharp.Shared;
+
+namespace WpfApp1
+{
+    /// <summary>
+    ///     Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Core.Initialize();
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows;
+using LibVLCSharp.Shared;
+
+namespace WpfApp1
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly LibVLC _libVlc;
+        private readonly MediaPlayer _mediaPlayer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _libVlc = new LibVLC();
+            _mediaPlayer = new MediaPlayer(_libVlc);
+
+            // we need the VideoView to be fully loaded before setting a MediaPlayer on it.
+            VideoView.Loaded += (sender, e) => VideoView.MediaPlayer = _mediaPlayer;
+            Unloaded += VideoView_Unloaded;
+        }
+
+        private void VideoView_Unloaded(object sender, RoutedEventArgs e)
+        {
+            _mediaPlayer.Stop();
+            _mediaPlayer.Dispose();
+            _libVlc.Dispose();
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!VideoView.MediaPlayer.IsPlaying)
+                //"rtmp://192.168.7.239:1935/live/stream"
+                using (var media = new Media(_libVlc,
+                           new Uri(
+                               "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")))
+                {
+                    VideoView.MediaPlayer.Play(media);
+                }
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (VideoView.MediaPlayer.IsPlaying) VideoView.MediaPlayer.Stop();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            VideoView.Dispose();
+        }
+    }
+}
