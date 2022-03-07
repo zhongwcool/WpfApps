d78abac274fa303ab7f0de99b4ee5a57cc02a877
Alex zhong <zhongwcool@163.com>
2022-03-07 18:21:28 +0800

    WpfApp8: 增加IconCheckBox用于实现ToolBar

---
+++ WpfApp8/Control/IconCheckBox.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfApp8.Control;
+
+public class IconCheckBox : CheckBox
+{
+    public static readonly DependencyProperty IconProperty =
+        DependencyProperty.Register("Icon", typeof(Geometry), typeof(IconCheckBox), new PropertyMetadata(null));
+
+
+    static IconCheckBox()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(IconCheckBox),
+            new FrameworkPropertyMetadata(typeof(IconCheckBox)));
+    }
+
+    public Geometry Icon
+    {
+        get => (Geometry)GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+}
