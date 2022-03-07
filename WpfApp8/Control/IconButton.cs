cae26fbabc9a9e5a92adb0f27f710cc7725d2aa6
Alex zhong <zhongwcool@163.com>
2022-03-07 16:42:10 +0800

    WpfApp8: 增加IconButton用于实现ToolBar

---
+++ WpfApp8/Control/IconButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfApp8.Control;
+
+public class IconButton : Button
+{
+    public static readonly DependencyProperty IconProperty =
+        DependencyProperty.Register("Icon", typeof(Geometry), typeof(IconButton), new PropertyMetadata(null));
+
+
+    static IconButton()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(IconButton),
+            new FrameworkPropertyMetadata(typeof(IconButton)));
+    }
+
+    public Geometry Icon
+    {
+        get => (Geometry)GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+}
