62c1d66e5669a1fd6e2be63433e8c363ec7ceec7
Alex zhong <zhongwcool@163.com>
2022-03-09 16:42:36 +0800

    WpfApp8: 独立布局文件以简化布局复杂度

---
+++ WpfApp8/Views/SidePanelRight.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace WpfApp8.Views;
+
+public partial class SidePanelRight : UserControl
+{
+    public SidePanelRight()
+    {
+        InitializeComponent();
+    }
+}
