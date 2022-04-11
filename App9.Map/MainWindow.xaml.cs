ea2a713418c0bba6226d79a0afd4f096d70bf861
Alex zhong <zhongwcool@163.com>
2022-04-11 16:19:54 +0800

    增加 App9：地图Demo

---
+++ App9.Map/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+
+namespace App9.Map;
+
+public partial class MainWindow : Window
+{
+    private readonly string _sUrl = AppDomain.CurrentDomain.BaseDirectory + "gd.htm";
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        WebBrowser.Loaded += (sender, args) =>
+        {
+            var uri = new Uri(this._sUrl);
+            WebBrowser.Navigate(uri);
+        };
+    }
+
+    private void ButtonChangeCenter_OnClick(object sender, RoutedEventArgs e)
+    {
+        WebBrowser.InvokeScript("randomZoomCenter");
+    }
+
+    private void ButtonAddPolygon_OnClick(object sender, RoutedEventArgs e)
+    {
+        WebBrowser.InvokeScript("setMapCenter", new object[] { 120.721603, 31.367004 });
+    }
+}
