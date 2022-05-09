9a71121e26c5aab0c0a216642ccec78411c81b4a
Alex zhong <zhongwcool@163.com>
2022-05-09 18:43:02 +0800

    App09：增加BingMap显示方法

---
+++ App09.Map/BingMapWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using Microsoft.Maps.MapControl.WPF;
+
+namespace App09.Map;
+
+public partial class BingMapWindow : Window
+{
+    public BingMapWindow()
+    {
+        InitializeComponent();
+        MyMap.Center = new Location(29.5617, 120.0962);
+    }
+}
