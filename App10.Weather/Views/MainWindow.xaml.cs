c347ba99054ccc1a9c3e32e506af43f3b7ded0e1
Alex zhong <zhongwcool@163.com>
2022-05-09 18:46:23 +0800

    App10：增加LineChart

--- App10.Weather/Views/MainWindow.xaml.cs
+++ App10.Weather/Views/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Linq;
+using System.Windows;
 using App10.Weather.ViewModels;
 
 namespace App10.Weather.Views;
@@ -12,5 +14,10 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         DataContext = MainViewModel.CreateInstance();
+
+        var x = Enumerable.Range(0, 1001).Select(i => i / 10.0).ToArray();
+        var y = x.Select(v => Math.Abs(v) < 1e-10 ? 1 : Math.Sin(v) / v).ToArray();
+        linegraph.Plot(x, y); // x and y are IEnumerable<double>
+        linegraph1.Plot(x, y); // x and y are IEnumerable<double>
     }
 }
