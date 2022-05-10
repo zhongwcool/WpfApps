1a545d46fad6c495b73c2ae79e90f72a5da474a0
Alex zhong <zhongwcool@163.com>
2022-05-10 11:50:34 +0800

    App09：在Xaml中配置地图参数

--- App09.Map/BingMapWindow.xaml.cs
+++ App09.Map/BingMapWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System.Windows;
-using Microsoft.Maps.MapControl.WPF;
 
 namespace App09.Map;
 
@@ -8,6 +7,5 @@ public partial class BingMapWindow : Window
     public BingMapWindow()
     {
         InitializeComponent();
-        MyMap.Center = new Location(29.5617, 120.0962);
     }
 }
