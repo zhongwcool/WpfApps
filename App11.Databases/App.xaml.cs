c0c914ba295f8d0254b14de93155e87ebacb00a1
Alex zhong <zhongwcool@163.com>
2022-06-02 18:21:38 +0800

    App11: 数据库使用

---
+++ App11.Databases/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace App11.Databases
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static string DbPath { get; set; } = @"DataSource=tt2.db";
+    }
+}
---
+++ App11.Databases/Views/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+using System.Data.SQLite;
+using System.Windows;
+
+namespace App11.Databases.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var connection = new SQLiteConnection(App.DbPath);
+            var datatable = new DataTable();
+            var adp = new SQLiteDataAdapter("SELECT * FROM User", connection);
+            adp.Fill(datatable);
+            DataGrid.ItemsSource = datatable.DefaultView;
+        }
+    }
+}
