4364834478cf82875aaef246d50ce53ee97dcdd9
alex.js <zhongwcool@163.com>
2023-05-12 17:19:51 +0800

    App16:增加机制预防图表因太多数据而无法绘制

---
+++ App16.Python/Data/CircularQueue.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App16.Python.Data;
+
+public class CircularQueue<T>
+{
+    private readonly T[] _items;
+    private int _head;
+    private int _tail;
+
+    public CircularQueue(int capacity)
+    {
+        _items = new T[capacity];
+        _head = -1;
+        _tail = -1;
+    }
+
+    public void Enqueue(T item)
+    {
+        // if (IsFull())
+        //     throw new OverflowException("Queue overflow.");
+
+        if (IsEmpty())
+            _head = 0;
+
+        _tail = (_tail + 1) % _items.Length;
+        _items[_tail] = item;
+    }
+
+    public T Dequeue()
+    {
+        // if (IsEmpty())
+        //     throw new InvalidOperationException("Queue is empty.");
+
+        T item = _items[_head];
+
+        if (_head == _tail)
+        {
+            _head = -1;
+            _tail = -1;
+        }
+        else
+        {
+            _head = (_head + 1) % _items.Length;
+        }
+
+        return item;
+    }
+
+    public T Peek()
+    {
+        if (IsEmpty())
+            throw new InvalidOperationException("Queue is empty.");
+
+        return _items[_head];
+    }
+
+    public bool IsEmpty()
+    {
+        return _head == -1;
+    }
+
+    public bool IsFull()
+    {
+        return ((_tail + 1) % _items.Length) == _head;
+    }
+
+    public int Count()
+    {
+        if (IsEmpty())
+            return 0;
+
+        return (_tail >= _head ? (_tail - _head) : (_items.Length - _head + _tail + 1));
+    }
+
+    public List<T> GetList()
+    {
+        var list = new List<T>();
+        if (IsEmpty())
+            return list;
+
+        var index = _head;
+        while (index != _tail)
+        {
+            list.Add(_items[index]);
+            index = (index + 1) % _items.Length;
+        }
+
+        list.Add(_items[_tail]);
+        return list;
+    }
+}
--- App16.Python/ViewModels/MainViewModel.cs
+++ App16.Python/ViewModels/MainViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
 using System.Windows.Threading;
+using App16.Python.Data;
 using CommunityToolkit.Mvvm.ComponentModel;
 using LiveCharts;
 using LiveCharts.Wpf;
@@ -18,7 +19,7 @@ public class MainViewModel : ObservableObject
 
     public MainViewModel()
     {
-        AxisXMax = 10;
+        AxisXMax = TabelShowCount - 1;
         AxisXMin = 0;
         AxisYMax = 10;
         AxisYMin = 0;
@@ -41,7 +42,26 @@ public class MainViewModel : ObservableObject
 
     private void ValueListOnCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
     {
-        ValueListCount = ValueList.Count;
+        var valueListCount = ValueList.Count;
+
+        if (valueListCount >= TabelCapacity)
+        {
+            Labels.RemoveRange(0, valueListCount - TabelShowCount);
+            ValueList.CollectionChanged -= ValueListOnCollectionChanged;
+            ValueList.Clear();
+            ValueList.AddRange(_shadowValue.GetList());
+            AxisXMax = TabelShowCount - 1;
+            AxisXMin = 0;
+            ValueList.CollectionChanged += ValueListOnCollectionChanged;
+            return;
+        }
+
+        //y轴的设置
+        if (valueListCount >= TabelShowCount)
+        {
+            AxisXMax = valueListCount - 1;
+            AxisXMin = valueListCount - TabelShowCount;
+        }
     }
 
     private readonly Random _random = new();
@@ -96,17 +116,23 @@ public class MainViewModel : ObservableObject
 
     //表中最大容纳个数
     private const int TabelShowCount = 10;
+    private readonly CircularQueue<double> _shadowValue = new(TabelShowCount);
+    private const int TabelCapacity = 100;
 
     //曲线图的总个数
-    private int ValueListCount = 0;
+    // private int ValueListCount = 0;
 
     public void Timer_Tick(object? sender, EventArgs e)
     {
         Dispatcher.CurrentDispatcher.Invoke(() =>
         {
+            //更新横坐标时间
+            Labels.Add(DateTime.Now.ToString("HH:mm:ss.fff"));
+
             var trend = _random.Next(-10, 100);
             //向图表中添加数据
             ValueList.Add(trend);
+            _shadowValue.Enqueue(trend);
 
             //确保Y轴曲线不会超过图表
             var maxY = (int)ValueList.Max();
@@ -115,16 +141,6 @@ public class MainViewModel : ObservableObject
             //Y轴保持数据居中（曲线会上下晃动）
             //int minY = ValueList.Count == 1 ? 0 : (int)ValueList.Min();
             //AxisYMin = minY - 10;
-
-            //y轴的设置
-            if (ValueList.Count >= TabelShowCount)
-            {
-                AxisXMax = ValueListCount - 1;
-                AxisXMin = ValueListCount - TabelShowCount;
-            }
-
-            //更新横坐标时间
-            Labels.Add(DateTime.Now.ToString("HH:mm:ss.fff"));
         });
     }
 }
