f35520577fb9bfa05ca730cf639e8da699009a94
alex.js <zhongwcool@163.com>
2023-05-24 18:41:14 +0800

    App05:增加列表互动效果

--- App05.TaskAndCancel/ViewModels/MainViewModel.cs
+++ App05.TaskAndCancel/ViewModels/MainViewModel.cs
@@ -1,30 +1,51 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 namespace App05.TaskAndCancel.ViewModels;
 
 public class MainViewModel : ObservableObject
 {
-    private static MainViewModel _instance;
+    private static MainViewModel? _instance;
+    private CancellationTokenSource _tokenSource = new();
 
     private MainViewModel()
     {
-        CommandConnect = new RelayCommand(() => { });
+        CommandCancel = new RelayCommand(() =>
+        {
+            _tokenSource.Cancel();
+            Devices.Add($"取消了Task.Wait: {DateTime.Now}\n");
+        });
+
+        CommandRenew = new RelayCommand(() =>
+        {
+            _tokenSource.Dispose();
+            _tokenSource = new CancellationTokenSource();
+            Task.Factory.StartNew(Test);
+        });
+
+        Task.Factory.StartNew(Test);
     }
 
-    public static MainViewModel CreateInstance()
+    public static MainViewModel? CreateInstance()
     {
         _instance ??= new MainViewModel();
         return _instance;
     }
 
-    public IRelayCommand CommandConnect { get; }
-
-    private string _txtToday;
-
-    public string TxtToday
+    private void Test()
     {
-        get => _txtToday;
-        set => SetProperty(ref _txtToday, value);
+        Application.Current.Dispatcher.Invoke(() => Devices.Add($"开始: {DateTime.Now}\n"));
+        Task.WaitAny(new[] { Task.Delay(10000, _tokenSource.Token) }, _tokenSource.Token);
+        Application.Current.Dispatcher.Invoke(() => Devices.Add($"结束: {DateTime.Now}\n"));
     }
+
+    public IRelayCommand CommandCancel { get; }
+    public IRelayCommand CommandRenew { get; }
+
+    public ObservableCollection<string> Devices { get; } = new();
 }
--- App05.TaskAndCancel/Views/MainWindow.xaml.cs
+++ App05.TaskAndCancel/Views/MainWindow.xaml.cs
@@ -1,44 +1,13 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using App05.TaskAndCancel.ViewModels;
 
 namespace App05.TaskAndCancel.Views;
 
-/// <summary>
-///     Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
-    private CancellationTokenSource _tokenSource = new();
-
     public MainWindow()
     {
         InitializeComponent();
         DataContext = MainViewModel.CreateInstance();
-
-        Task.Factory.StartNew(Test);
-    }
-
-    private void Test()
-    {
-        Dispatcher.Invoke(() => { Info.AppendText($"开始: {DateTime.Now}\n"); });
-        Task.WaitAny(new[] { Task.Delay(10000, _tokenSource.Token) },
-            _tokenSource.Token);
-        Dispatcher.Invoke(() => { Info.AppendText($"结束: {DateTime.Now}\n"); });
-    }
-
-    private void ButtonStop_OnClick(object sender, RoutedEventArgs e)
-    {
-        _tokenSource.Cancel();
-        Info.AppendText($"取消了Task.Wait: {DateTime.Now}\n");
-    }
-
-    private void ButtonRenew_OnClick(object sender, RoutedEventArgs e)
-    {
-        _tokenSource.Dispose();
-        _tokenSource = new CancellationTokenSource();
-        Task.Factory.StartNew(Test);
     }
 }
