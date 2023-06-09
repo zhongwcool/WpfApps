990c011b51aee7c9d7e6ca48a81c06884b8a96fe
alex.js <zhongwcool@163.com>
2023-06-09 15:53:40 +0800

    增加App17模拟登录并请求数据

---
+++ App17.Login/Models/ZySession.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace App17.Login.Models;
+
+public class ZySession
+{
+    public string Cookie { set; get; }
+    public string UserPhone { set; get; }
+    public string UserPwd { set; get; }
+    public string DeviceId { set; get; }
+    public string LoginApi { set; get; }
+    public string RedirApi { set; get; }
+    public string WaterApi { set; get; }
+}
+
+public class ZySessionModel
+{
+    public List<ZySession> Sessions { set; get; }
+    public string Last_Update { set; get; }
+}
---
+++ App17.Login/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace App17.Login.ViewModels;
+
+public class MainWindowViewModel : ObservableObject
+{
+}
