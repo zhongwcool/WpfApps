54cc8ea630abf615032545ef2381284d4dcf56fd
alex.js <zhongwcool@163.com>
2023-08-24 16:48:19 +0800

    App16:优化LiveCharts的使用

--- App16.Python/ViewModels/MainViewModel.cs
+++ App16.Python/ViewModels/MainViewModel.cs
@@ -2,7 +2,6 @@
 using System.Collections.ObjectModel;
 using System.Windows.Threading;
 using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
 using LiveChartsCore;
 using LiveChartsCore.Defaults;
 using LiveChartsCore.Drawing;
@@ -14,55 +13,33 @@ namespace App16.Python.ViewModels;
 
 public partial class MainViewModel : ObservableObject
 {
-    private readonly ObservableCollection<DateTimePoint> _observableValues;
-    public ObservableCollection<ISeries> Series { get; set; }
+    private ObservableCollection<DateTimePoint> RawSeries { get; }
+    public ObservableCollection<ISeries> XYSeries { get; set; }
 
     private readonly Random _random = new();
 
-    [RelayCommand]
-    public void AddItem()
+    private void AddOneRand()
     {
-        var randomValue = _random.Next(1, 100);
-        _observableValues.Add(new DateTimePoint(DateTime.Now, randomValue));
-    }
-
-    [RelayCommand]
-    public void RemoveItem()
-    {
-        if (_observableValues.Count == 0) return;
-        _observableValues.RemoveAt(0);
+        var rand = _random.Next(1, 100);
+        RawSeries.Add(new DateTimePoint(DateTime.Now, rand));
+        while (RawSeries.Count > 10) RawSeries.RemoveAt(0);
     }
 
     public void AddData(int ret)
     {
-        _observableValues.Add(new DateTimePoint(DateTime.Now, ret));
-        RemoveItem();
+        RawSeries.Add(new DateTimePoint(DateTime.Now, ret));
+        while (RawSeries.Count > 100) RawSeries.RemoveAt(0);
     }
 
     public MainViewModel()
     {
-        // Use ObservableCollections to let the chart listen for changes (or any INotifyCollectionChanged). // mark
-        _observableValues = new ObservableCollection<DateTimePoint>
-        {
-            // new(DateTime.Now.Subtract(TimeSpan.FromMilliseconds(12000)), 0),
-            // new(DateTime.Now.Subtract(TimeSpan.FromMilliseconds(11000)), 0),
-            // new(DateTime.Now.Subtract(TimeSpan.FromMilliseconds(10000)), 0),
-            // new(DateTime.Now.Subtract(TimeSpan.FromMilliseconds(9000)), 0),
-            new(DateTime.Now.Subtract(TimeSpan.FromMilliseconds(8000)), 0),
-            new(DateTime.Now.Subtract(TimeSpan.FromMilliseconds(7000)), 0),
-            new(DateTime.Now.Subtract(TimeSpan.FromMilliseconds(6000)), 0),
-            new(DateTime.Now.Subtract(TimeSpan.FromMilliseconds(5000)), 0),
-            new(DateTime.Now.Subtract(TimeSpan.FromMilliseconds(4000)), 0),
-            new(DateTime.Now.Subtract(TimeSpan.FromMilliseconds(3000)), 0),
-            new(DateTime.Now.Subtract(TimeSpan.FromMilliseconds(2000)), 0),
-            new(DateTime.Now.Subtract(TimeSpan.FromMilliseconds(1000)), 0),
-        };
-
-        Series = new ObservableCollection<ISeries>()
+        RawSeries = new ObservableCollection<DateTimePoint>();
+        AddOneRand();
+        XYSeries = new ObservableCollection<ISeries>
         {
             new LineSeries<DateTimePoint>
             {
-                Values = _observableValues,
+                Values = RawSeries,
                 GeometrySize = 4f,
                 DataPadding = new LvcPoint(0, 0)
             }
@@ -77,11 +54,6 @@ public partial class MainViewModel : ObservableObject
             LabelsRotation = 15,
             TextSize = 12,
             SeparatorsPaint = new SolidColorPaint(SKColors.LightGray, 1),
-
-            // when using a date time type, let the library know your unit // mark
-            UnitWidth = TimeSpan.FromMilliseconds(1000).Ticks,
-            // The MinStep property forces the separator to be greater than 1 day.
-            MinStep = TimeSpan.FromMilliseconds(50).Ticks // mark
         }
     };
 
@@ -93,21 +65,11 @@ public partial class MainViewModel : ObservableObject
             TextSize = 10,
             MinLimit = 0,
             MaxLimit = 100,
-
-            // when using a date time type, let the library know your unit // mark
-            UnitWidth = 10,
-
-            // The MinStep property forces the separator to be greater than 1 day.
-            MinStep = 1 // mark
         }
     };
 
     public void Timer_Tick(object? sender, EventArgs e)
     {
-        Dispatcher.CurrentDispatcher.Invoke(() =>
-        {
-            RemoveItem();
-            AddItem();
-        });
+        Dispatcher.CurrentDispatcher.Invoke(AddData);
     }
 }
