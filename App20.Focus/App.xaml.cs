cbc5d4b7b9159afe86ffe74606a60889c7deb15d
alex.js <zhongwcool@163.com>
2023-09-12 16:01:52 +0800

    App20:增加打印系统信息

--- App20.Focus/App.xaml.cs
+++ App20.Focus/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Management;
 using System.Windows;
 using Serilog;
 
@@ -19,7 +21,41 @@ public partial class App : Application
                 rollOnFileSizeLimit: true,
                 outputTemplate: "{Timestamp:HH:mm:ss.fff} [{Level:u3}] {Message:l}{NewLine}{Exception}")
             .CreateLogger();
-        Log.Fatal("Hello, Serilog!");
+        PrintSystemInfo();
+    }
+
+    private static void PrintSystemInfo()
+    {
+        Log.Fatal("Windows Version: {OsVersion}", Environment.OSVersion);
+        Log.Fatal(".NET SDK Version: {Version}", Environment.Version);
+
+        ManagementObjectSearcher searcher;
+
+        // Query CPU
+        searcher = new ManagementObjectSearcher("select * from Win32_Processor");
+        foreach (var o in searcher.Get())
+        {
+            var share = (ManagementObject)o;
+            Log.Fatal("CPU: {Unknown}", share["Name"]);
+        }
+
+        // Query Graphics Card
+        searcher = new ManagementObjectSearcher("select * from Win32_VideoController");
+        foreach (var o in searcher.Get())
+        {
+            var share = (ManagementObject)o;
+            Log.Fatal("Graphics Card: " + share["Name"]);
+        }
+
+        // Query Memory
+        searcher = new ManagementObjectSearcher("select * from Win32_PhysicalMemory");
+        foreach (var o in searcher.Get())
+        {
+            var share = (ManagementObject)o;
+            var capacityBytes = (ulong)share["Capacity"];
+            var mem = (double)capacityBytes / 1024 / 1024 / 1024;
+            Log.Fatal("Memory: " + mem + "GB");
+        }
     }
 
     protected override void OnExit(ExitEventArgs e)
