760d13b8272adbda6a07d35668e0c441da20e2c4
alex.js <zhongwcool@163.com>
2023-10-13 16:05:47 +0800

    增加App14:Scroll

--- App14.IASystem/Models/Owner.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace App14.IASystem.Models;
-
-public class Owner
-{
-    public Pool Pool { get; set; }
-    public string Description { get; set; }
-}
---
+++ App14.Scroll/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace App14.Scroll;
+
+/// <summary>
+///     Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
