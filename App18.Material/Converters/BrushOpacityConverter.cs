2b5e3ef8dc0e6a013c9652b264653be740b6ffd7
alex.js <zhongwcool@163.com>
2024-06-12 18:13:50 +0800

    降低界面颜色的对比度

--- App18.Material/Converters/BrushOpacityConverter.cs
+++ App18.Material/Converters/BrushOpacityConverter.cs
@@ -7,7 +7,7 @@ namespace App18.Material.Converters;
 
 public class BrushOpacityConverter : IValueConverter
 {
-    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
         if (value is not SolidColorBrush brush) return null;
         var opacity = System.Convert.ToDouble(parameter, CultureInfo.InvariantCulture);
@@ -17,7 +17,7 @@ public class BrushOpacityConverter : IValueConverter
         };
     }
 
-    public object? ConvertBack(object value, Type targetType, object? parameter, CultureInfo culture)
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
         return Binding.DoNothing;
     }
